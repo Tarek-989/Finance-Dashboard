99a6aef9c363567c0a1db11c2cd6980ee68b7942
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2023-03-31 15:14:13 +0300

    add type for Get Transactions Response

--- client/src/state/types.ts
+++ client/src/state/types.ts
@@ -8,6 +8,16 @@ export interface GetProductsResponse {
 	createdAt: string;
 	updatedAt: string;
 }
+export interface GetTransactionsResponse {
+	id: string;
+	_id: string;
+	__v: number;
+	buyer: number;
+	amount: number;
+	productIds: Array<string>;
+	createdAt: string;
+	updatedAt: string;
+}
 export interface GetKpisResponse {
 	id: string;
 	_id: string;
