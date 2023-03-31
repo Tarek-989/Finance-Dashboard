ece0d9b86769c24f5f1e5e0153eb78f99f814c7e
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2023-03-31 15:07:34 +0300

    push transaction data to DB

--- server/index.js
+++ server/index.js
@@ -7,9 +7,10 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import kpiRoutes from './routes/kpi.js';
 import productRoutes from './routes/product.js';
+import Transaction from './models/Transaction.js';
 import Product from './models/Product.js';
 import KPI from './models/KPI.js';
-import { kpis, products } from './data/data.js';
+import { kpis, products, transactions } from './data/data.js';
 
 /* CONFIGURATIONS */
 dotenv.config()
@@ -40,5 +41,6 @@ mongoose
 		// await mongoose.connection.db.dropDatabase();
 		// KPI.insertMany(kpis);
 		// Product.insertMany(products);
+		// Transaction.insertMany(transactions);
 	})
 	.catch((error) => console.log(`server error: ${error}`));
