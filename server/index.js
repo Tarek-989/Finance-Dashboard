db70da32282310688ce94a02c94ed8a1f387bac6
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2023-03-31 12:45:53 +0300

    push data to DB

--- server/index.js
+++ server/index.js
@@ -6,8 +6,9 @@ import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import kpiRoutes from './routes/kpi.js';
+import Product from './models/Product.js';
 import KPI from './models/KPI.js';
-import { kpis } from './data/data.js';
+import { kpis, products } from './data/data.js';
 
 /* CONFIGURATIONS */
 dotenv.config()
@@ -36,5 +37,6 @@ mongoose
 		//! ADD DATA ONE TIME ONLY  
 		// await mongoose.connection.db.dropDatabase();
 		// KPI.insertMany(kpis);
+		// Product.insertMany(products);
 	})
 	.catch((error) => console.log(`server error: ${error}`));
